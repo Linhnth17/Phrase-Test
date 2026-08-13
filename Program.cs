e2648c66b6f6ceeb9e0c68203adffd90989e5cda
Linhnth17 <linhn7899@gmail.com>
2026-08-13 17:01:39 +0700

    Add WelcomeMessage string to en resx
    Test change to trigger the Phrase Repo Sync auto-import on main.

--- Program.cs
+++ Program.cs
@@ -7,3 +7,4 @@ var resources = new ResourceManager("Test_phrase.Resources.Resources", typeof(Pr
 Console.WriteLine(resources.GetString("Greeting", CultureInfo.InvariantCulture));
 Console.WriteLine(resources.GetString("Farewell", CultureInfo.InvariantCulture));
 Console.WriteLine(resources.GetString("OrderConfirmation", CultureInfo.InvariantCulture));
+Console.WriteLine(resources.GetString("WelcomeMessage", CultureInfo.InvariantCulture));
