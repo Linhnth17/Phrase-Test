2419bef002557fdbe2ac6ed7fe76df89bbdb6a8a
Linhnth17 <linhn7899@gmail.com>
2026-08-19 14:08:47 +0700

    Test 3 scenarios: edit existing translated key, delete key, add new key
    - WelcomeMessage: English text changed (already has a human-verified
      es translation) - does the translation survive or get overwritten?
    - Farewell: removed entirely - does it disappear from Phrase or stay
      orphaned (no --cleanup used)?
    - AutoTranslateTest3: brand new key - should get an MT draft.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,5 @@ using System.Resources;
 var resources = new ResourceManager("Test_phrase.Properties.Resources", typeof(Program).Assembly);
 
 Console.WriteLine(resources.GetString("Greeting", CultureInfo.InvariantCulture));
-Console.WriteLine(resources.GetString("Farewell", CultureInfo.InvariantCulture));
 Console.WriteLine(resources.GetString("OrderConfirmation", CultureInfo.InvariantCulture));
 Console.WriteLine(resources.GetString("WelcomeMessage", CultureInfo.InvariantCulture));
